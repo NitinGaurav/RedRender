c4ba9d797e35750b0a34fa70aedabeef83738847
nitinreddoorz <nitin.gaurav@reddoorz.com>
2024-08-12 13:47:02 +0530

    added database

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.2].define(version: 2024_08_12_064616) do
-  create_table "posts", force: :cascade do |t|
+  create_table "posts", charset: "utf8", force: :cascade do |t|
     t.string "name"
     t.text "message"
     t.datetime "created_at", null: false
